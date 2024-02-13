2535be3c585f8868316aa8277f2279bde612d188
Igor Korolevich <ikorolevich4@gmail.com>
2024-02-13 16:47:30 +0300

    4 ready

---
+++ Task4/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace Task4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>();
+
+            FileInfo studentFile = new FileInfo("D:\\students.dat");
+
+            if (studentFile.Exists)
+            {
+                FileStream studentStream = studentFile.OpenRead();
+                using (BinaryReader br = new BinaryReader(studentStream))
+                {
+                    while (studentStream.Position < studentStream.Length)
+                    {
+                        Student student = new Student();
+                        student.Name = br.ReadString();
+                        student.Group = br.ReadString();
+                        student.DateOfBirth = DateTime.FromBinary(br.ReadInt64());
+                        student.AverageScore = br.ReadDecimal();
+                        students.Add(student);
+                    }
+
+                }
+
+            }
+            else Console.WriteLine("File does not exist");
+
+            DirectoryInfo workDirectory = new DirectoryInfo("D:\\Students");
+
+            if (!workDirectory.Exists)
+            {
+                workDirectory.Create();
+            }
+
+            foreach (Student student in students)
+            {
+                string fileGroupPath = workDirectory.FullName + "\\" + student.Group + ".txt";
+                FileInfo NewFile = new FileInfo(fileGroupPath);
+                if (!NewFile.Exists)
+                {
+                    NewFile.Create().Close(); 
+                }
+                
+                using (StreamWriter sw = NewFile.AppendText())
+                {
+                    sw.Write(student.Name);
+                    sw.Write('\t');
+                    sw.Write(student.DateOfBirth);
+                    sw.Write('\t');
+                    sw.Write(student.AverageScore);
+                    sw.Write('\n');
+                }
+
+            }
+        }
+    }
+}
---
+++ Task4/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace Task4
+{
+    internal class Student
+    {
+        public string Name { get; set; }
+        public string Group { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public decimal AverageScore { get; set; }
+    }
+}
