1ec8ff2d5f2f0a1b09b16e53cc13ce6fcdf0e85b
Igor Korolevich <ikorolevich4@gmail.com>
2024-02-13 21:57:56 +0300

    final

---
+++ Directory.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+
+static long getDirectorySize(DirectoryInfo dir)
+{
+    long len = 0;
+    FileInfo[] files;
+
+
+    DirectoryInfo[] dirs = dir.GetDirectories();
+
+    if (dirs.Length == 0)
+    {
+        files = dir.GetFiles();
+        foreach (FileInfo file in files)
+        {
+            len += file.Length;
+        }
+
+        return len;
+    }
+    files = dir.GetFiles();
+    foreach (FileInfo file in files)
+    {
+        len += file.Length;
+    }
+    foreach (var item in dirs)
+    {
+        len += getDirectorySize(item);
+    }
+    return len;
+}
+
+public class Class1
+{
+	public Class1()
+	{
+	}
+}
